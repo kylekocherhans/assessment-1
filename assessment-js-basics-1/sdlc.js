ec301c2d16e743b2ef9a15e0b6ef8821836ec17e
Kyle Kocherhans <kylekocherhans@gmail.com>
2022-12-16 17:52:48 -0700

    initial commit

---
+++ assessment-js-basics-1/sdlc.js
@@ -0,0 +1,27 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step. Developers work with stakeholders to gather requirements that the planned software needs to meet. These requirements include what the software needs to do, what features it should have, and what resources it requires.";
+const design = "This is the third step. Developers plan out what the software will look like, how it will interact with other systems, and how users will interact with it.";
+const implementation = "This is the fourth step. This is where the coding happens and the software is actually built following plans from previous steps.";
+const testingAndIntegration = "This is the fifth step. In this step the software that was created is tested to ensure it works as expected and satisfies the requirements created during the planning phases. Integration is the release and deployment of the software for use.";
+const maintain = "This is the sixth step. This step involves the upkeep of the software which includes fixing bugs and any other updates that are required.";
